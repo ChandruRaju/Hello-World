0fdacf6868883ff27b23bf09a611c64c2048e289
ChandruRaju <51356176+ChandruRaju@users.noreply.github.com>
2019-06-04 23:16:00 +0530

    Update odd_even.py

---
+++ odd_even.py
@@ -0,0 +1,7 @@
+c=int(input(""))
+if(c%2==0):
+  print("Even")
+elif(c%2!=0):
+  print("Odd")
+else:
+  print("Invalid")
--- postive_negative_zero.py
+++
@@ -1,7 +0,0 @@
-c=int(input(""))
-if(c>0):
-  print("Positive")
-elif(c<0):
-  print("Negative")
-else:
-  print("Zero")
